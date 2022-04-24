4456f0f0071763b1a928b8a6f651070cc728c121
Bob Feuerstein <thebobbyllama@yahoo.com>
2022-04-23 21:12:07 -0500

    Added custom font styling.

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=BioRhyme&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
